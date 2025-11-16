968a00c3f88ce86e4f81bad8063a95924c92c9f1
omegalpha-37 <3305425884@qq.com>
2025-11-16 16:49:51 +0800

    98767890

--- functions/routes/auth.js
+++ functions/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const supabase = require('../routes/supabaseClient');
+const supabase = require('./routes/supabaseClient');
 
 // 注册
 router.post('/register', async (req, res) => {
